d974d5fb001d41166bb717dbc11a535c706bcd61
msamufidi <munafsamufidi11@gmail.com>
2022-10-07 08:01:46 +0700

    change button

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -23,7 +23,7 @@ class _MyAppState extends State<MyApp> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Text("Return from req"),
+              Text("Return from req res"),
               MaterialButton(
                   child: Text(
                     "POST",
